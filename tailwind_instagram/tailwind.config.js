33fc43fc69ec4a23321ecabe534d85a76ccf2870
Alexsander Ribeiro <62142077+alexsanderneto@users.noreply.github.com>
2025-02-11 13:42:05 -0300

    Cópia do Login/Senha do instagram!
    Sobre uma referência para treinamento em tailwind.

---
+++ tailwind_instagram/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      Padrao: "cdcfc0",
+      Secundaria: "f1ebe5",
+    },
+  },
+  plugins: [],
+};
